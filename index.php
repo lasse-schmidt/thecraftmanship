d0f3988c0e407a611b6003dcaaaa5d0415890ddf
Lasse Schmidt <l.s10600@gmail.com>
2024-02-26 11:12:24 +0100

    added core.css and linked them in index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Meta tags-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="core.css">
+
+    <!--Title-->
     <title>thecraftmanship</title>
 </head>
 <body>
