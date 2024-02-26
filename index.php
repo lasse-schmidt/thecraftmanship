36a9f5c7a8ba0a447ef7a7372aa63c3e3cb2c28b
Lasse Schmidt <l.s10600@gmail.com>
2024-02-26 11:13:56 +0100

    fixed css link, and added a background color.

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!--CSS-->
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="core.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/core.css">
 
     <!--Title-->
     <title>thecraftmanship</title>
